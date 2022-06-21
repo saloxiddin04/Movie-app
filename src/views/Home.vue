10295aede25bdb324dff7a99c1b32685762014b6
Saloxiddin701 <abduraxmon.69list.ru@gmail.com>
2022-06-21 14:57:27 +0500

    dist

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -46,7 +46,7 @@ export default {
 
     const searchMovies = () => {
       if (search.value != '') {
-        fetch(`http://www.omdbapi.com/?apikey=${env.apikey}&s=${search.value}`)
+        fetch(`https://www.omdbapi.com/?apikey=${env.apikey}&s=${search.value}`)
             .then(response => response.json())
             .then(data => {
               movies.value = data.Search
